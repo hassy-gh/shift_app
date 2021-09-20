a88af97187ce2fa34f6d4495abf44ba68deca7dd
hassy-gh <shufc0107@gmail.com>
2021-09-20 06:10:20 +0000

    Finish layout and routes

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -12,7 +12,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   end
   
   test "should get home" do
-    get static_pages_home_url
+    get root_path
     assert_response :success
     assert_select "title", @base_title
   end
