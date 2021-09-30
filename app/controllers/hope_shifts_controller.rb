992d78daadf6b8d6f375e3bc801784054baf3d11
hassy-gh <shufc0107@gmail.com>
2021-09-30 08:37:02 +0000

    Improve hope shift

--- app/controllers/hope_shifts_controller.rb
+++ app/controllers/hope_shifts_controller.rb
@@ -2,12 +2,13 @@ class HopeShiftsController < ApplicationController
   
   def new
     @hope_shift = current_user.hope_shifts.build
+    @day = params[:format]
   end
   
   def create
     @hope_shift = current_user.hope_shifts.build(hope_shift_params)
     if @hope_shift.save
-      flash[:success] = "シフト希望を登録しました"
+      flash[:success] = "登録しました"
       redirect_to current_user
     else
       render 'new'
