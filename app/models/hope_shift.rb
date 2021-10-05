512ed8345c2cd3592d6f90360542a01d3733b712
hassy-gh <shufc0107@gmail.com>
2021-10-05 15:46:52 +0900

    Improve simple calendar

--- app/models/hope_shift.rb
+++ app/models/hope_shift.rb
@@ -4,4 +4,12 @@ class HopeShift < ApplicationRecord
   with_options on: :create do
     validates :start_time, uniqueness: { scope: :user_id }
   end
+  validate :required_either_content_or_time
+  
+  private
+  
+    def required_either_content_or_time
+      return if content.present? ^ hope_start_time.present? || hope_end_time.present?
+      errors.add(:base, "どちらか一方を入力してください")
+    end
 end
