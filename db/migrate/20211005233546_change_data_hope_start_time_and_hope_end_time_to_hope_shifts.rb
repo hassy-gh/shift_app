bac72dcf14d59159a2199b4fba0e94d8efcb1bc9
hassy-gh <shufc0107@gmail.com>
2021-10-07 20:20:08 +0900

    Improve simple calendar

--- db/migrate/20211005233546_change_data_hope_start_time_and_hope_end_time_to_hope_shifts.rb
+++
@@ -1,6 +0,0 @@
-class ChangeDataHopeStartTimeAndHopeEndTimeToHopeShifts < ActiveRecord::Migration[6.0]
-  def change
-    change_column :hope_shifts, :hope_start_time, :string
-    change_column :hope_shifts, :hope_end_time, :string
-  end
-end
