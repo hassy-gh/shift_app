36f112391251448ac03fcdccc3102102c204b2ec
hassy-gh <shufc0107@gmail.com>
2021-10-21 17:57:36 +0900

    Improve form

--- app/controllers/fixed_shifts_controller.rb
+++ app/controllers/fixed_shifts_controller.rb
@@ -1,5 +1,5 @@
 class FixedShiftsController < ApplicationController
-  before_action :get_group, only: [:index, :new, :create, :edit, :update]
+  before_action :get_group, only: [:index, :new, :create, :edit, :update, :day_index]
   before_action :logged_in_user, only: [:index, :new, :create, :edit, :update, :destroy]
   before_action :admin_user, only: [:index, :new, :create, :edit, :update, :destroy]
   
@@ -10,30 +10,35 @@ class FixedShiftsController < ApplicationController
   def new
     @day = params[:format]
     @hope_shifts = @group.hope_shifts.where(start_time: @day)
-    # count = Group.find(current_user.group_id).users.count
-    # count.times do
-      # @fixed_shift = FixedShift.new
-      @form = Form::FixedShiftCollection.new
-    # end
+    @fixed_shift = FixedShift.new
+    # @form = Form::FixedShiftCollection.new
   end
   
   def create
-    # @fixed_shift = FixedShift.new(fixed_shift_params)
-    @form = Form::FixedShiftCollection.new(fixed_shift_collection_params)
-    if @form.save
-    # if @fixed_shift.save
-      flash[:success] = "登録しました"
-      redirect_to fixed_shifts_path
+    @fixed_shift = FixedShift.find_or_initialize_by(start_time: params[:start_time], user_id: params[:user_id])
+    if @fixed_shift.new_record?
+      @fixed_shift = FixedShift.new(fixed_shift_params)
+      if @fixed_shift.save
+        flash[:success] = "登録しました"
+        redirect_to fixed_shifts_path
+      else
+        @hope_shifts = @group.hope_shifts.where(start_time: params[:fixed_shift][:start_time])
+        render 'new'
+      end
     else
-      @hope_shifts = @group.hope_shifts.where(start_time: 
-              params[:form_fixed_shift_collection][:fixed_shifts_attributes]["0"][:start_time])
-      render 'new'
+      if @fixed_shift.update!(fixed_shift_params)
+        flash[:success] = "変更しました"
+        redirect_to fixed_shifts_path
+      else
+        @hope_shifts = @group.hope_shifts.where(start_time: params[:fixed_shift][:start_time])
+        render 'new'
+      end
     end
   end
   
   def edit
     @fixed_shift = FixedShift.find(params[:id])
-    @hope_shifts = @group.hope_shifts..where(start_time: @fixed_shift.start_time)
+    @hope_shifts = @group.hope_shifts.where(start_time: @fixed_shift.start_time)
   end
   
   def update
@@ -52,18 +57,20 @@ class FixedShiftsController < ApplicationController
   
   def day_index
     @day = params[:format]
-    @fixed_shifts = FixedShift.where(start_time: @day)
+    @users = @group.users
+    @fixed_shifts = @group.fixed_shifts.where(start_time: @day)
+    @hope_shifts = @group.hope_shifts.where(start_time: @day)
   end
   
   private
   
-    # def fixed_shift_params
-    #   params.require(:fixed_shift).permit(:user_id, :start_time, :fixed_start_time, :fixed_end_time)
-    # end
-    
-    def fixed_shift_collection_params
-      params
-        .require(:form_fixed_shift_collection)
-        .permit(fixed_shifts_attributes: [:user_id, :start_time, :fixed_start_time, :fixed_end_time])
+    def fixed_shift_params
+      params.require(:fixed_shift).permit(:user_id, :start_time, :fixed_start_time, :fixed_end_time)
     end
+    
+    # def fixed_shift_collection_params
+    #   params
+    #     .require(:form_fixed_shift_collection)
+    #     .permit(fixed_shifts_attributes: [:user_id, :start_time, :fixed_start_time, :fixed_end_time])
+    # end
 end
