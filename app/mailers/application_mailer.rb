8357b8e8a9999e9e10ea52806bea3524fdba6c63
hassy-gh <shufc0107@gmail.com>
2021-12-09 15:38:36 +0900

    Add Sendgrid to heroku

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'noreply@example.com', charset: 'ISO-2022-JP'
+  default from: 'noreply@shiftapp2021.com', charset: 'ISO-2022-JP'
   layout 'mailer'
 end
