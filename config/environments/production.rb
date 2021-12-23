6ce9a261af868624cb020057ad4e0040f2a2048e
hassy-gh <shufc0107@gmail.com>
2021-12-23 16:59:40 +0900

    Add sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,19 +66,19 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_caching = false
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   host = 'www.shiftapp2021.com'
   config.action_mailer.default_url_options = { host: host }
-  ActionMailer::Base.smtp_settings = {
-    :port           => 587,
-    :address        => 'smtp.sendgrid.net',
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => host,
-    :authentication => :plain,
-    :enable_starttls_auto => true,
+  ActionMailer::Base.smtp_settings         = {
+      address:              'smtp.sendgrid.net',
+      user_name:            'apikey',
+      password:             ENV['SENDGRID_APIKEY'],
+      authentication:       :plain,
+      domain:               'heroku.com',
+      port:                 587,
+      enable_starttls_auto: true,
   }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
