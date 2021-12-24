cd0854f9f46437b77a78896dee884d0061f086eb
hassy-gh <shufc0107@gmail.com>
2021-12-24 09:12:11 +0900

    Modify sendgrid settings and layouts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index, :show, :edit, :update, :destroy]
-  before_action :no_join_user, except: [:new, :create]
+  before_action :no_join_user, only: [:index, :show]
   before_action :get_user, only: [:show, :edit, :update, :destroy, :correct_user]
   before_action :get_group, only: :index
   before_action :correct_user, only: [:show, :edit, :update]
